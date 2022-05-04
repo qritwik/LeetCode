a437b7300b4f6fa2edf8292d608fc5cd83a92c04
Ritwik Raj <qritwik@gmail.com>
2022-05-04 22:40:41 +0530

    Time: 1 ms (88.89%), Space: 53.8 MB (60.58%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -4,32 +4,25 @@ class Solution {
         int m = matrix.length;
         int n = matrix[0].length;
         
+        int a[] = new int[m];
+        int b[] = new int[n];
+        
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
                 if(matrix[i][j] == 0) {
-                    setMatZero(matrix,i,j,m,n);
+                    a[i] = -1;
+                    b[j] = -1;
                 }
             }
         }
         
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-                if(matrix[i][j] == -999) {
-                   matrix[i][j] = 0;
+                if(a[i] == -1 || b[j] == -1) {
+                    matrix[i][j] = 0;
                 }
             }
         }
         
-    }
-    
-    public void setMatZero(int[][] matrix, int si, int sj, int m, int n) {
-        for(int j = 0; j < n; j++) {
-            if(matrix[si][j] != 0)
-            matrix[si][j] = -999;
-        }
-        for(int i = 0; i < m; i++) {
-            if(matrix[i][sj] != 0)
-            matrix[i][sj] = -999;
-        }
     }
 }
