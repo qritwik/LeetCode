c8422c3d6eb54e4280a2a0ce7f18972e4d928988
Ritwik Raj <qritwik@gmail.com>
2022-05-22 12:22:23 +0530

    Time: 2 ms (82.04%), Space: 40.4 MB (84.26%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -0,0 +1,22 @@
+class Solution {
+
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else if (stack.empty()) {
+                stack.push(ch);
+            } else if (ch == ')' && stack.peek() == '(') {
+                stack.pop();
+            } else if (ch == '}' && stack.peek() == '{') {
+                stack.pop();
+            } else if (ch == ']' && stack.peek() == '[') {
+                stack.pop();
+            } else {
+                stack.push(ch);
+            }
+        }
+        return stack.empty();
+    }
+}
