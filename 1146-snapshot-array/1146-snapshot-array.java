79a2af4269d481024af3fef1173dc49ad96bbc73
Ritwik Raj <qritwik@gmail.com>
2022-07-02 23:19:09 +0530

    Time: 59 ms (55.60%), Space: 84 MB (38.51%) - LeetHub

--- 1146-snapshot-array/1146-snapshot-array.java
+++ 1146-snapshot-array/1146-snapshot-array.java
@@ -1,28 +1,27 @@
 class SnapshotArray {
-    
     int snapId;
     HashMap<Integer, Integer>[] map;
 
     public SnapshotArray(int length) {
         snapId = 0;
         map = new HashMap[length];
-        for(int i = 0; i < length; i++) {
+        for (int i = 0; i < length; i++) {
             map[i] = new HashMap<>();
         }
     }
-    
+
     public void set(int index, int val) {
         map[index].put(snapId, val);
     }
-    
+
     public int snap() {
         snapId++;
         return snapId - 1;
     }
-    
+
     public int get(int index, int snap_id) {
-        while(snap_id >= 0) {
-            if(map[index].containsKey(snap_id)) {
+        while (snap_id >= 0) {
+            if (map[index].containsKey(snap_id)) {
                 return map[index].get(snap_id);
             }
             snap_id--;
@@ -30,7 +29,6 @@ class SnapshotArray {
         return 0;
     }
 }
-
 /**
  * Your SnapshotArray object will be instantiated and called as such:
  * SnapshotArray obj = new SnapshotArray(length);
