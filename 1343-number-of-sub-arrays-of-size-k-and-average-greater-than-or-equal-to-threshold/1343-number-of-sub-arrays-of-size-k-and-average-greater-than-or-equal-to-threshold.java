0d7884279b407be6ed6440a738702b1a7fc6c987
Ritwik Raj <qritwik@gmail.com>
2022-07-03 12:59:04 +0530

    Time: 6 ms (33.25%), Space: 69.9 MB (36.32%) - LeetHub

--- 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.java
+++ 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.java
@@ -1,21 +1,22 @@
 class Solution {
+
     public int numOfSubarrays(int[] arr, int k, int threshold) {
         int count = 0;
         int i = 0;
-        int j = i + k - 1 ;
+        int j = i + k - 1;
         int sum = 0;
-        for(int c = i; c <= j; c++) {
+        for (int c = i; c <= j; c++) {
             sum += arr[c];
         }
         int avg = sum / k;
-        if(avg >= threshold) count++;
-        for(int t = 0; t < arr.length-k; t++) {
+        if (avg >= threshold) count++;
+        for (int t = 0; t < arr.length - k; t++) {
             int eleToRemove = arr[t];
             sum -= eleToRemove;
-            int eleToAdd = arr[t+k];
+            int eleToAdd = arr[t + k];
             sum += eleToAdd;
             avg = sum / k;
-            if(avg >= threshold) count++;
+            if (avg >= threshold) count++;
         }
         return count;
     }
