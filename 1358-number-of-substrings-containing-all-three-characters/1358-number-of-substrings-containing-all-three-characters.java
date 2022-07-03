20a29fbb43585e5a8b57e399421921ef82458261
Ritwik Raj <qritwik@gmail.com>
2022-07-03 15:27:24 +0530

    Time: 65 ms (19.33%), Space: 51.5 MB (15.81%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
@@ -1,43 +1,41 @@
 class Solution {
+
     public int numberOfSubstrings(String s) {
         int count = 0;
         int i = 0;
         int j = 2;
         char[] arr = s.toCharArray();
         HashMap<Character, Integer> map = new HashMap<>();
-        for(int k = i; k <= j; k++) {
+        for (int k = i; k <= j; k++) {
             insertIntoMap(map, arr[k]);
         }
-        while(j < arr.length) {
-            if(map.size() == 3) {
-                count += (arr.length-j);
+        while (j < arr.length) {
+            if (map.size() == 3) {
+                count += (arr.length - j);
                 removeFromMap(map, arr[i]);
                 i++;
-            }
-            else {
+            } else {
                 j++;
-                if(j < arr.length) {
+                if (j < arr.length) {
                     insertIntoMap(map, arr[j]);
                 }
             }
         }
         return count;
     }
-    
+
     public void insertIntoMap(HashMap<Character, Integer> map, Character key) {
-        if(!map.containsKey(key)) {
+        if (!map.containsKey(key)) {
             map.put(key, 1);
-        }
-        else {
+        } else {
             map.put(key, map.get(key) + 1);
         }
     }
-    
+
     public void removeFromMap(HashMap<Character, Integer> map, Character key) {
-        if(map.get(key) == 1) {
+        if (map.get(key) == 1) {
             map.remove(key);
-        }
-        else {
+        } else {
             map.put(key, map.get(key) - 1);
         }
     }
