033d2b7a3067a4962c9c2576a9e636019e1c17c5
Ritwik Raj <qritwik@gmail.com>
2022-07-16 17:39:01 +0530

    Time: 35 ms (87.58%), Space: 61.3 MB (87.24%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.java
+++ 239-sliding-window-maximum/239-sliding-window-maximum.java
@@ -1,22 +1,22 @@
 class Solution {
+
     public int[] maxSlidingWindow(int[] A, int B) {
         int i = 0;
         int j = 0;
-        Deque<Integer> deque = new LinkedList<Integer>();        
-        int[] result = new int[A.length-B+1];
+        Deque<Integer> deque = new LinkedList<Integer>();
+        int[] result = new int[A.length - B + 1];
         int k = 0;
-        while(j < A.length) {
-            while(!deque.isEmpty() && A[deque.peekLast()] < A[j]) {
+        while (j < A.length) {
+            while (!deque.isEmpty() && A[deque.peekLast()] < A[j]) {
                 deque.pollLast();
             }
             deque.add(j);
-            if(j-i+1 < B) {
+            if (j - i + 1 < B) {
                 j++;
-            }
-            else if(j-i+1 == B) {
+            } else if (j - i + 1 == B) {
                 int mi = deque.peek();
                 result[k++] = A[mi];
-                while(!deque.isEmpty() && deque.peek() <= i) {
+                while (!deque.isEmpty() && deque.peek() <= i) {
                     deque.poll();
                 }
                 i++;
@@ -26,4 +26,3 @@ class Solution {
         return result;
     }
 }
-
