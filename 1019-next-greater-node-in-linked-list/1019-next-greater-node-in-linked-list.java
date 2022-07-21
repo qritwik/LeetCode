efcdb22ce4737df42b8072267fab6f4a674e3aa2
Ritwik Raj <qritwik@gmail.com>
2022-07-22 01:06:39 +0530

    Time: 61 ms (45.25%), Space: 69.3 MB (63.67%) - LeetHub

--- 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.java
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.java
@@ -9,38 +9,37 @@
  * }
  */
 class Solution {
+
     public int[] nextLargerNodes(ListNode head) {
         ArrayList<Integer> list = new ArrayList<>();
         ListNode temp = head;
-        while(temp != null) {
+        while (temp != null) {
             list.add(temp.val);
             temp = temp.next;
         }
-        
+
         int[] res = new int[list.size()];
-        
+
         Stack<Integer> stack = new Stack<>();
-        for(int i = 0; i < list.size(); i++) {
-            if(stack.isEmpty()) {
+        for (int i = 0; i < list.size(); i++) {
+            if (stack.isEmpty()) {
                 stack.push(i);
-            }
-            else if(list.get(i) <= list.get(stack.peek())) {
+            } else if (list.get(i) <= list.get(stack.peek())) {
                 stack.push(i);
-            }
-            else {
-                while(!stack.isEmpty() && (list.get(i) > list.get(stack.peek()))) {
+            } else {
+                while (!stack.isEmpty() && (list.get(i) > list.get(stack.peek()))) {
                     int idx = stack.pop();
                     res[idx] = list.get(i);
                 }
                 stack.push(i);
             }
         }
-        
-        while(!stack.isEmpty()) {
+
+        while (!stack.isEmpty()) {
             int idx = stack.pop();
             res[idx] = 0;
         }
-        
+
         return res;
     }
 }
