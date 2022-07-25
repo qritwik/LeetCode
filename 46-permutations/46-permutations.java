3c0cd2ae036f3496864e4da07519a8ae7d6e79d0
Ritwik Raj <qritwik@gmail.com>
2022-07-26 01:03:27 +0530

    Time: 2 ms (76.41%), Space: 44.9 MB (30.51%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,20 +1,20 @@
 class Solution {
+
     public List<List<Integer>> permute(int[] nums) {
         return permuteHelper(nums, 0);
     }
-    
+
     public List<List<Integer>> permuteHelper(int[] nums, int si) {
-        
         List<List<Integer>> result = new ArrayList<>();
-        if(si == nums.length) {
+        if (si == nums.length) {
             List<Integer> tempList = new ArrayList<>();
             result.add(tempList);
             return result;
         }
-        
-        List<List<Integer>> smallOut = permuteHelper(nums, si+1);
-        for(List<Integer> list : smallOut) {
-            for(int i = 0; i < nums.length-si; i++) {
+
+        List<List<Integer>> smallOut = permuteHelper(nums, si + 1);
+        for (List<Integer> list : smallOut) {
+            for (int i = 0; i < nums.length - si; i++) {
                 List<Integer> tempList = new ArrayList<>(list);
                 tempList.add(i, nums[si]);
                 result.add(tempList);
