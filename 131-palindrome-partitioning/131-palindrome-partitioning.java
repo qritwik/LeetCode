630af47cdac03869a6454e5e306a2a0d6ca199ad
Ritwik Raj <qritwik@gmail.com>
2022-07-28 03:00:12 +0530

    Time: 73 ms (5.12%), Space: 176.3 MB (65.07%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.java
+++ 131-palindrome-partitioning/131-palindrome-partitioning.java
@@ -1,24 +1,18 @@
 class Solution {
+
     public List<List<String>> partition(String s) {
-        
         List<List<String>> result = new ArrayList<>();
-        if(s.length() == 0) {
+        if (s.length() == 0) {
             List<String> list = new ArrayList<>();
             result.add(list);
             return result;
         }
-        if(s.length() == 1) {
-            List<String> list = new ArrayList<>();
-            list.add(s);
-            result.add(list);
-            return result;
-        }
-        
-        for(int i = 0; i < s.length(); i++) {
-            String prefix = s.substring(0, i+1);
-            if(isPalindrome(prefix)) {
-                List<List<String>> smallAns = partition(s.substring(i+1));
-                for(List<String> list : smallAns) {
+
+        for (int i = 0; i < s.length(); i++) {
+            String prefix = s.substring(0, i + 1);
+            if (isPalindrome(prefix)) {
+                List<List<String>> smallAns = partition(s.substring(i + 1));
+                for (List<String> list : smallAns) {
                     List<String> newList = new ArrayList<>(list);
                     newList.add(0, prefix);
                     result.add(newList);
@@ -27,12 +21,12 @@ class Solution {
         }
         return result;
     }
-    
+
     public boolean isPalindrome(String s) {
         int li = 0;
         int ri = s.length() - 1;
-        while(li < ri) {
-            if(s.charAt(li) != s.charAt(ri)) {
+        while (li < ri) {
+            if (s.charAt(li) != s.charAt(ri)) {
                 return false;
             }
             li++;
