a818eb93cd523b48cd0227c5cf514432acb13103
Ritwik Raj <qritwik@gmail.com>
2022-07-31 22:46:43 +0530

    Time: 2 ms (23.40%), Space: 43.7 MB (63.07%) - LeetHub

---
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        
+        if(root == null) {
+            return true;
+        }
+        
+        if(root.left == null && root.right == null) return true;
+        
+        if(isValidBST(root.left) && isValidBST(root.right) && getMax(root.left) < root.val && getMin(root.right) > root.val) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    
+    public long getMax(TreeNode root) {
+        if(root == null) {
+            return Long.MIN_VALUE;
+        }
+        if(root.left == null && root.right == null) {
+            return root.val;
+        }
+        long l = getMax(root.left);
+        long r = getMax(root.right);
+        return Math.max(root.val, Math.max(l, r));
+    }
+    
+    public long getMin(TreeNode root) {
+        if(root == null) {
+            return Long.MAX_VALUE;
+        }
+        if(root.left == null && root.right == null) {
+            return root.val;
+        }
+        long l = getMin(root.left);
+        long r = getMin(root.right);
+        return Math.min(root.val, Math.min(l, r));
+    }
+}
