9100d3eedbfda50f09524b60f26a37e3a804beb3
Ritwik Raj <qritwik@gmail.com>
2022-08-07 23:41:55 +0530

    Time: 1 ms (99.86%), Space: 44.1 MB (42.89%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -26,24 +26,16 @@ class Solution {
             Arrays.fill(row, Integer.MAX_VALUE);
         
         // start from the top of the triangle
-        return recurse(triangle, 0, 0, memo);
+        return func(triangle, 0, 0, triangle.size(), memo);
     }
     
-    private int recurse(List<List<Integer>> triangle, int row, int col, int[][] memo) {
-        // base case
-        if (row == triangle.size())
-            return 0;
-        
-        // check dp table
-        if (memo[row][col] != Integer.MAX_VALUE)
-            return memo[row][col];
-        
-        // either go to [row + 1, col] or [row + 1, col + 1]
-        int remain = Math.min(recurse(triangle, row + 1, col, memo),
-                              recurse(triangle, row + 1, col + 1, memo));
-        
-        // combine answer with current value
-        memo[row][col] = triangle.get(row).get(col) + remain;
-        return memo[row][col];
+    public int func(List<List<Integer>> triangle, int row, int col, int n, int[][] dp) {
+        if (row == n - 1) {
+            return triangle.get(row).get(col);
+        }
+        if (dp[row][col] != Integer.MAX_VALUE) return dp[row][col];
+        int first = func(triangle, row + 1, col, n, dp);
+        int second = func(triangle, row + 1, col + 1, n, dp);
+        return dp[row][col] = Math.min(first, second) + triangle.get(row).get(col);
     }
 }
