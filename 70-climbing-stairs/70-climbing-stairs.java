58e2dfef4e5de78b3a70a39cd9af339e7bd292de
Ritwik Raj <qritwik@gmail.com>
2022-08-07 15:42:42 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (67.14%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,18 +1,14 @@
 class Solution {
-    int dp[];
+
     public int climbStairs(int n) {
-        dp = new int[n+1];
-        return climbStairsHelper(n);
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+        return func(n, dp);
     }
-    
-    public int climbStairsHelper(int n) {
-        if(n == 0 || n == 1) {
-            return 1;
-        }
-        
-        if(dp[n] != 0) return dp[n];
-        
-        dp[n] = climbStairsHelper(n-1) + climbStairsHelper(n-2);
-        return dp[n];
+
+    public int func(int n, int[] dp) {
+        if (n == 0 || n == 1) return 1;
+        if (dp[n] != -1) return dp[n];
+        return dp[n] = func(n - 1, dp) + func(n - 2, dp);
     }
 }
