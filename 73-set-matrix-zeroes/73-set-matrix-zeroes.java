814dcc0d3f246ef5822fd1d678a7246997d180c8
Ritwik Raj <qritwik@gmail.com>
2022-09-11 22:55:35 +0530

    Time: 2 ms (48.42%), Space: 54.4 MB (21.27%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -1,32 +1,40 @@
 class Solution {
+
     public void setZeroes(int[][] matrix) {
-        boolean fr = false, fc = false;
-        for(int i = 0; i < matrix.length; i++) {
-            for(int j = 0; j < matrix[0].length; j++) {
-                if(matrix[i][j] == 0) {
-                    if(i == 0) fr = true;
-                    if(j == 0) fc = true;
-                    matrix[i][0] = 0;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean fr = false;
+        boolean fc = false;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
                     matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                    if (i == 0) fr = true;
+                    if (j == 0) fc = true;
                 }
             }
         }
-        for(int i = 1; i < matrix.length; i++) {
-            for(int j = 1; j < matrix[0].length; j++) {
-                if(matrix[i][0] == 0 || matrix[0][j] == 0) {
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[0][j] == 0 || matrix[i][0] == 0) {
                     matrix[i][j] = 0;
                 }
             }
         }
-        if(fr) {
-            for(int j = 0; j < matrix[0].length; j++) {
+
+        if (fr) {
+            for (int j = 0; j < n; j++) {
                 matrix[0][j] = 0;
             }
         }
-        if(fc) {
-            for(int i = 0; i < matrix.length; i++) {
+
+        if (fc) {
+            for (int i = 0; i < m; i++) {
                 matrix[i][0] = 0;
             }
         }
-    }   
+    }
 }
