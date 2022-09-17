877bf8857f29782868655522f8f282b09f7d555a
Ritwik Raj <qritwik@gmail.com>
2022-09-17 23:11:28 +0530

    Time: 4 ms (100.00%), Space: 40.8 MB (47.76%) - LeetHub

---
+++ 371-sum-of-two-integers/371-sum-of-two-integers.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int getSum(int a, int b) {
+        
+        String binaryStrA = Integer.toBinaryString(a);
+        String binaryStrB = Integer.toBinaryString(b);
+        
+        char[] ca = binaryStrA.toCharArray();
+        char[] cb = binaryStrB.toCharArray();
+        int i = ca.length - 1;
+        int j = cb.length - 1;
+        
+        String res = "";
+        int c = 0;
+        while(i >= 0 || j >= 0) {
+            int sum = c;
+            if(i >= 0 && ca[i] == '1') {
+                sum += 1;
+            }
+            if(j >= 0 && cb[j] == '1') {
+                sum += 1;
+            }
+            if(sum == 1) {
+                res = "1" + res;
+                c = 0;
+            }
+            else if(sum == 2) {
+                res = "0" + res;
+                c = 1;
+            }
+            else if(sum == 0){
+                res = "0" + res;
+                c = 0;
+            }
+            else {
+                res = "1" + res;
+                c = 1;
+            }
+            i--;
+            j--;
+        }
+        res = String.valueOf(c) + res;
+        
+        int out = (int)Long.parseUnsignedLong(res, 2);
+        return out;
+    }
+}
