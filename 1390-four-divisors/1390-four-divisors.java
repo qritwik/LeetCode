70dfc3ace92ceddaa5b868a5135d94f099d60cbf
Ritwik Raj <qritwik@gmail.com>
2022-09-18 02:30:01 +0530

    Time: 106 ms (18.60%), Space: 48.6 MB (24.92%) - LeetHub

--- 1390-four-divisors/1390-four-divisors.java
+++ 1390-four-divisors/1390-four-divisors.java
@@ -3,9 +3,6 @@ class Solution {
         int sum = 0;
         for (int num : nums) {
             Pair p = getCountofFactors(num);
-            System.out.println(p.count);
-            System.out.println(p.sum);
-            System.out.println("-------");
             if (p.count == 4) {
                 sum += p.sum;
             }
@@ -15,16 +12,15 @@ class Solution {
 
     public Pair getCountofFactors(int n) {
         Pair p = new Pair();
-        for (int i = 2; i*i <= n; i++) {
+        for (int i = 2; i * i <= n; i++) {
             if (n % i == 0) {
-                if(n/i == i) {
+                if (n / i == i) {
                     Pair p1 = new Pair();
                     p1.sum += i;
                     p1.count++;
                     return p1;
-                }
-                else {
-                    p.sum += i + n/i;
+                } else {
+                    p.sum += i + n / i;
                     p.count = p.count + 2;
                 }
             }
