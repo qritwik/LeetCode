9f941669e6f83b77dab74c1e4690a9ca509736f3
Ritwik Raj <qritwik@gmail.com>
2022-09-18 13:59:10 +0530

    Time: 1 ms (98.65%), Space: 41.1 MB (60.13%) - LeetHub

--- 202-happy-number/202-happy-number.java
+++ 202-happy-number/202-happy-number.java
@@ -1,29 +1,25 @@
 class Solution {
     public boolean isHappy(int n) {
-    
         int slow = n;
         int fast = cal(n);
-
-        while(fast != 1){
-            
+        
+        while(fast != 1) {
             slow = cal(slow);
             fast = cal(cal(fast));
-            
-            if (slow == fast){
+            if(slow == fast) {
                 return false;
             }
         }
         return true;
     }
     
-    public int cal(int num){
+    public int cal(int num) {
         int sum = 0;
-        while(num > 0){
+        while(num > 0) {
             int d = num % 10;
-            sum = sum + d*d;
+            sum += d * d;
             num = num / 10;
         }
         return sum;
     }
-    
 }
