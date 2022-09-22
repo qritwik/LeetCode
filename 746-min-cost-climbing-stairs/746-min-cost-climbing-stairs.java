d320771a088ab81de6fac05d14b469c520da5d9b
Ritwik Raj <qritwik@gmail.com>
2022-09-22 09:40:03 +0530

    Time: 2 ms (32.23%), Space: 43.4 MB (64.88%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -1,15 +1,19 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
-        int[] dp = new int[cost.length+1];
+        int[] dp = new int[cost.length + 1];
         Arrays.fill(dp, -1);
         return Math.min(func(cost, 0, dp), func(cost, 1, dp));
     }
-    
+
     public int func(int[] cost, int start, int[] dp) {
-        if(start >= cost.length) return 0;
-        if(dp[start] != -1) return dp[start];
-        int left = cost[start] + func(cost, start+1, dp);
-        int right = cost[start] + func(cost, start+2, dp);
-        return dp[start] = Math.min(left, right);
+        if (start >= cost.length) {
+            return 0;
+        }
+        if (dp[start] != -1) {
+            return dp[start];
+        }
+        int out1 = cost[start] + func(cost, start + 1, dp);
+        int out2 = cost[start] + func(cost, start + 2, dp);
+        return dp[start] = Math.min(out1, out2);
     }
 }
