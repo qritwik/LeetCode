fd0cb7c859b79d9ff0feec9a8c0a06fe4983bee4
Ritwik Raj <qritwik@gmail.com>
2022-09-22 12:12:26 +0530

    Time: 2 ms (6.63%), Space: 41.5 MB (48.81%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.java
+++ 213-house-robber-ii/213-house-robber-ii.java
@@ -1,19 +1,25 @@
 class Solution {
-
     public int rob(int[] nums) {
         if (nums.length == 1) return nums[0];
-        int[][] dp = new int[nums.length + 1][nums.length + 1];
-        for (int[] row : dp) {
+        int n = nums.length;
+        int[][] dp = new int[n+1][n+1];
+        for(int[] row : dp) {
             Arrays.fill(row, -1);
         }
-        return Math.max(func(nums, 0, nums.length - 2, dp), func(nums, 1, nums.length - 1, dp));
+        return Math.max(func(nums, 0, n-2, dp), func(nums, 1, n-1, dp));
     }
-
+    
     public int func(int[] nums, int low, int high, int[][] dp) {
-        if (low > high) return 0;
-        if (dp[low][high] != -1) return dp[low][high];
-        int first = func(nums, low + 1, high, dp);
-        int second = func(nums, low + 2, high, dp) + nums[low];
-        return dp[low][high] = Math.max(first, second);
+        if(low > high) {
+            return 0;
+        }
+        
+        if(dp[low][high] != -1) {
+            return dp[low][high];
+        }
+        
+        int out1 = func(nums, low + 1, high, dp);
+        int out2 = nums[low] + func(nums, low + 2, high, dp);
+        return dp[low][high] = Math.max(out1, out2);
     }
 }
