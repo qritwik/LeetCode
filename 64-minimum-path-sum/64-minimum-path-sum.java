615cf12fcce8e04909b7c30824e1b6e806b10243
Ritwik Raj <qritwik@gmail.com>
2022-09-22 19:16:59 +0530

    Time: 1 ms (99.97%), Space: 44.7 MB (98.64%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -1,24 +1,35 @@
 class Solution {
     public int minPathSum(int[][] grid) {
-        int m = grid.length; int n = grid[0].length;
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m+1][n+1];
+        for(int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+        int min_sum = 0;
+        min_sum = func(grid, 0, m-1, 0, n-1, dp);
+        return min_sum;
+    }
+    
+    public int func(int[][] grid, int is, int ie, int js, int je, int[][] dp) {
         
-        int[][] dp = new int[m][n];
-        dp[0][0] = grid[0][0];
-        
-        for (int i = 1; i < m; i++) {
-            dp[i][0] = grid[i][0] + dp[i-1][0];
+        if(is == ie && js == je) {
+            return grid[is][js];
         }
         
-        for (int i = 1; i < n; i++) {
-            dp[0][i] = grid[0][i] + dp[0][i-1];
+        int out1 = Integer.MAX_VALUE;
+        int out2 = Integer.MAX_VALUE;
+        
+        if(dp[is][js] != -1) {
+            return dp[is][js];
         }
         
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                dp[i][j] = grid[i][j] + Math.min(dp[i-1][j], dp[i][j-1]);
-            }
-        }
+        if(is < ie)
+            out1 = grid[is][js] + func(grid, is + 1, ie, js, je, dp);
         
-        return dp[m-1][n-1];
+        if(js < je)
+            out2 = grid[is][js] + func(grid, is, ie, js + 1, je, dp);
+        
+        return dp[is][js] = Math.min(out1, out2);
     }
 }
