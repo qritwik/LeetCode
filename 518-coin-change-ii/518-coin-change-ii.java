0b33c67aaf7b511ec93f89d61c696986d70d1cc6
Ritwik Raj <qritwik@gmail.com>
2022-09-26 00:20:45 +0530

    Time: 10 ms (53.21%), Space: 48.6 MB (50.55%) - LeetHub

--- 518-coin-change-ii/518-coin-change-ii.java
+++ 518-coin-change-ii/518-coin-change-ii.java
@@ -1,30 +1,34 @@
 class Solution {
-
     public int change(int amount, int[] coins) {
+        
         int n = coins.length;
-        int[][] dp = new int[n + 1][amount + 1];
-        for (int[] row : dp) {
-            Arrays.fill(row, -1);
+        int[][] dp = new int[n+1][amount+1];
+        
+        // Initialisation
+        for(int i = 0; i < n+1; i++) {
+            for(int j = 0; j < amount+1; j++) {
+                if(i == 0) {
+                    dp[i][j] = 0;
+                }
+                if(j == 0) {
+                    dp[i][j] = 1;
+                }
+            }
         }
-        return func(coins, 0, amount, n, dp);
-    }
-
-    public int func(int[] coins, int i, int amount, int n, int[][] dp) {
-        if (amount == 0) {
-            return 1;
+        
+        // Tabulation
+        for(int i = 1; i < n+1; i++) {
+            for(int j = 1; j < amount+1; j++) {
+                if(coins[i-1] <= j) {
+                    dp[i][j] = dp[i][j-coins[i-1]] + dp[i-1][j];
+                }
+                else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
         }
-
-        if (i == n || amount < 0) {
-            return 0;
-        }
-
-        if (dp[i][amount] != -1) {
-            return dp[i][amount];
-        }
-
-        int out1 = func(coins, i, amount - coins[i], n, dp);
-        int out2 = func(coins, i + 1, amount, n, dp);
-
-        return dp[i][amount] = out1 + out2;
+        
+        return dp[n][amount];
+        
     }
 }
